a8c8a088b8c17e6eaee393d0b72ac4f4a39ac2c8
ressonix <michael@hostbend.net>
2020-12-10 20:45:01 -0500

    Initial Commit
    Tasks completed in this commit:
    - Created New Hugo Project
    - Created New Hugo Theme
    - Added hero for lander (light/dark schemes)

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-rocky-linux
+# rockylinux.org
+
+The Rocky Linux landing page, built using [Hugo](https://gohugo.io) and [TailwindCSS](https://tailwindcss.com).
+
+For now, you'll need to run `NODE_ENV=production hugo` for proper PurgeCSS to work in production.
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+    darkMode: 'media',
+    purge: {
+        content: ["./layouts/**/*.html", "./content/**/*.md", "./content/**/*.html"],
+    },
+    theme: {
+        extend: {
+            colors: {
+                gray: colors.gray,
+            },
+            fontFamily: {
+                display: ['Red Hat Display', 'sans-serif'],
+                sans: ['Red Hat Text', 'sans-serif'],
+            },
+        },
+    },
+    variants: {},
+    plugins: [],
+};
